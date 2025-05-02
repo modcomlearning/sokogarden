13155b521c018c7cb154901c60556457653ff2c0
modcomlearning <modcomlearning@gmail.com>
2025-05-02 11:29:49 +0300

    fIRST

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation, Link } from "react-router-dom";
 import Signup from './components/Signup';
 import Signin from './components/Signin';
 import Addproduct from './components/Addproduct';
@@ -15,7 +15,14 @@ function App() {
     <div className="App">
           <header className="App-header">
               <h1>SokoGarden - Buy & Sell Online</h1>
+          
           </header>
+          <nav className="m-4">
+              <Link to='/' className="btn btn-dark mx-2">HOME</Link>
+            <Link to='/addproduct' className="btn btn-dark mx-2">UPLOAD PRODUCTS</Link>
+            <Link to='/signin' className="btn btn-dark mx-2">Sign In</Link>
+            <Link to='/signup' className="btn btn-dark mx-2">Sign Up</Link>
+</nav>
            <Routes>
             <Route path="/signup" element={<Signup />} />
             <Route path="/signin" element={<Signin />} />
